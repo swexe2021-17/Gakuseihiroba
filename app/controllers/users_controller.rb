35f9a585c851420445c0dde9cd5e9afd0c174281
kiyo <1911140072w@ed.fuk.kindai.ac.jp>
2021-11-17 06:52:27 +0000

    ページ遷移　質問投稿＆ユーザ登録

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,9 +10,9 @@ class UsersController < ApplicationController
     end
     
     def create
-             @user = User.new(uid: params[:uid] ,
-             password: BCrypt::Password.create(params[:password]),
-             password_confirmation: params[:password_confirmation])
+             @user = User.new(uid: params[:user][:uid] ,
+             password: BCrypt::Password.create(params[:user][:password]),
+             password_confirmation: params[:user][:password_confirmation])
        if @user.save
            flash[:notice] =  'ユーザに登録しました'
        redirect_to root_path
