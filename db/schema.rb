77a157632aa96a938a7bc9541581da53fe7e1d4c
kiyo <1911140072w@ed.fuk.kindai.ac.jp>
2021-11-22 07:52:27 +0000

    質問詳細表示

--- db/schema.rb
+++ db/schema.rb
@@ -28,13 +28,6 @@ ActiveRecord::Schema.define(version: 2021_11_15_070914) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "user_creates", force: :cascade do |t|
-    t.string "uid"
-    t.string "pass"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "password"
     t.string "uid"
