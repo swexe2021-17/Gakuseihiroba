49f0462b15125d51336bab51915b22d24222f2ce
kiyo <1911140072w@ed.fuk.kindai.ac.jp>
2021-11-22 07:09:16 +0000

    question_show

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,12 @@
 class QuestionsController < ApplicationController
     
     def index
+        @users=User.all
         @questions=Question.all
+    end
+    
+    def show
+       @question=Question.find(params[:id]) 
     end
     
     def new
