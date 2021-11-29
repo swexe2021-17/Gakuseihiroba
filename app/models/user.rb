d1f10c6c2c4bf792aed642d24f389ff3430ccf12
kiyo <1911140072w@ed.fuk.kindai.ac.jp>
2021-11-29 08:02:42 +0000

    回答追加（回答表示未完了）

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     has_many :questions
+    has_many :answers
     validates :password, confirmation: true
     attr_accessor :password, :password_confirmation
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
 
     resources :users
     resources :questions
+    resources :answers
 end
