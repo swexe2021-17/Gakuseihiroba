22a909a509ed6428f58d45f2a0ca97f203c32ead
kiyo <1911140072w@ed.fuk.kindai.ac.jp>
2021-12-13 07:05:04 +0000

    ログアウト

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
     root 'users#index'
     get 'users/login_go'
+    get '/users/logout'
     post 'users/login' #usersコントローラのloginクラスを呼ぶ
+    
     
     resources :users
     resources :questions
